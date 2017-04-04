335a0243927dc8aee39b0b268d23d841e273e475
fuzhuo <fuzhuo233@gmail.com>
2017-04-04 10:33:21 +0800

    first version, webkit crash

---
+++ README.md
@@ -0,0 +1,6 @@
+# TVMLWebview
+
+Try call webview from TVML
+
+Next Step:
+Call js to get video link and play by native player instead of webview.
---
+++ TVMLWebkit/JSBridge.swift
@@ -0,0 +1,28 @@
+//
+//  JSBridge.swift
+//  TVMLWebkit
+//
+//  Created by zfu on 2017/4/4.
+//  Copyright © 2017年 zfu. All rights reserved.
+//
+import TVMLKit
+import Foundation
+@objc protocol JBExports: JSExport {
+    static func showBrowser() -> Void
+    static func log(_ str: String) -> Void
+}
+
+@objc class JSBridge : NSObject, JBExports {
+    static func showBrowser() {
+        let wc = WebViewController()
+        let appd = UIApplication.shared.delegate as! AppDelegate
+        let controller = appd.appController
+        controller?.navigationController.present(wc, animated: false, completion: {
+            //...
+            print("show toast")
+        })
+    }
+    static func log(_ str: String) -> Void {
+        print("jslog: \(str)")
+    }
+}
